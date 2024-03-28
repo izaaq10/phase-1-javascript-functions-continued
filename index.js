7001f6543fb98231bca446ec9d746d4ef6e657a9
Izaaq <odongoizaaq@gmail.com>
2024-03-28 08:33:28 -0400

    Adds the function-continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate'){
+ return`This Saturday, I want to ${activity}!`
+  
+};
+saturdayFun();
+saturdayFun('bathe my dog');
+
+
+function mondayWork(work = 'go to the office'){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(string = "*"){
+    return function (adjective = "a hard worker"){
+        return `You are ${string}${adjective}${string}!`
+}
+}
